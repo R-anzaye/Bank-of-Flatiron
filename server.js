1ab59c1a3c4e3bc8582ed1d30a382d369861cfe6
rileydollar8@gmail.com <ryan.anzaye@student.moringaschool.com>
2024-04-28 18:25:29 +0300

    server

---
+++ server.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server')
+
+const server = jsonServer.create()
+
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use('http://localhost:8001/transactions', router)
+server.listen(process.env.PORT || 8001, () => {
+  console.log('JSON Server is running')
+})
